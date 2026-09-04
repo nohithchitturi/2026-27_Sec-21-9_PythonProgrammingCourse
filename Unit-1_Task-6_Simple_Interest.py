1052819bec637825a8eab06a53663ec0228b8f64
nohithchitturi <teja.chitturi123@gmail.com>
2026-09-04 15:50:57 +0530

    simple interest

---
+++ Unit-1_Task-6_Simple_Interest.py
@@ -0,0 +1,6 @@
+#To calculate the simple intrest
+principal=float(input("Enter the principal amount in rupees:"))
+rate=float(input("Enter the rate interest in rupees:"))
+time=float(input("Enter the time in months:"))
+interest=(principal*rate*time)/100
+print(interest)
